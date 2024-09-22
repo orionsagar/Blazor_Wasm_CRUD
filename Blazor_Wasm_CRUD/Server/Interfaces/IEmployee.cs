900a174291b64ce9d46b24f143651804b4a10b0e
orionsagar <baruasgrps@yahoo.com>
2024-09-22 22:42:42 +0600

    First Commit

---
+++ Blazor_Wasm_CRUD/Server/Interfaces/IEmployee.cs
@@ -0,0 +1,20 @@
+﻿using Blazor_Wasm_CRUD.Server.Models;
+using System.Collections.Generic;
+
+namespace Blazor_Wasm_CRUD.Server.Interfaces
+{
+    public interface IEmployee
+    {
+        public List<Employee> GetAllEmployees();
+
+        public void AddEmployee(Employee employee);
+
+        public void UpdateEmployee(Employee employee);
+
+        public Employee GetEmployeeData(int id);
+
+        public void DeleteEmployee(int id);
+
+        public List<City> GetCity();
+    }
+}
---
+++ Blazor_Wasm_CRUD/Shared/Models/City.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Blazor_Wasm_CRUD.Server.Models
+{
+    public partial class City
+    {
+        public int CityId { get; set; }
+        public string CityName { get; set; }
+    }
+}
